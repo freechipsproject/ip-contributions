f118df671303e17db748921ea6d5fa53873fb923
Guy Hutchison <guy@dryv.io>
2020-02-13 09:50:36 -0800

    Changed tests to use random values instead of incrementing values.  Expanded tester to test ranges from 8b to 32b.

--- src/test/scala/chisel/lib/ecc/EccTester.scala
+++ src/test/scala/chisel/lib/ecc/EccTester.scala
@@ -5,29 +5,35 @@ import chisel3.iotesters.PeekPokeTester
 
 class EccTester(dut: EccPair) extends PeekPokeTester(dut) {
   val dutWidth = dut.getWidthParam
-
+  var testVal : Int = 0
   // send through some data without errors
   for (i <- 0 to 20) {
-    poke(dut.io.dataIn, i.U)
+    testVal = rnd.nextInt() & ((1 << dutWidth) - 1)
+
+    poke(dut.io.dataIn, testVal.U)
     poke(dut.io.errorLocation, 0.U)
     poke(dut.io.injectError, 0.U)
     step(1)
-    expect(dut.io.dataOut, i.U)
+    expect(dut.io.dataOut, testVal.U)
   }
 
   // inject single bit errors
   for (i <- 0 to dutWidth) {
+    testVal = rnd.nextInt() & ((1 << dutWidth) - 1)
+
     poke(dut.io.errorLocation, i.U)
     poke(dut.io.injectError, 1.U)
-    poke(dut.io.dataIn, "xFF".U)
+    poke(dut.io.dataIn, testVal.U)
     step(1)
-    expect(dut.io.dataOut, "xFF".U)
+    expect(dut.io.dataOut, testVal.U)
   }
 }
 
 object EccTester extends App {
-  iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new EccPair(8)) {
-    dut => new EccTester(dut)
+  for (width <- 8 to 32) {
+    iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new EccPair(width=width)) {
+      dut => new EccTester(dut)
+    }
   }
 }
 
